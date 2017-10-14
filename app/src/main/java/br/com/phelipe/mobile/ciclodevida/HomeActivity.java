478f88d60d767694375168e891f6bf033157c2ac
phelipelc <phelipelcampos@hotmail.com>
2017-10-14 20:48:05 -0300

    ciclos aplicação
    ciclos aplicação

--- app/src/main/java/br/com/phelipe/mobile/ciclodevida/HomeActivity.java
+++ app/src/main/java/br/com/phelipe/mobile/ciclodevida/HomeActivity.java
@@ -9,5 +9,64 @@ public class HomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+
+        System.out.println( "Método onCreate() executado." );
     }
-}
+
+    @Override
+     protected void onDestroy() {
+                 // TODO Auto-generated method stub
+                 super.onDestroy();
+
+                 System.out.println( "Método onDestroy() executado." );
+    }
+
+
+
+    @Override
+    protected void onPause() {
+                 // TODO Auto-generated method stub
+                 super.onPause();
+
+                 System.out.println( "Método onPause() executado." );
+    }
+     @Override
+     protected void onRestart() {
+                // TODO Auto-generated method stub
+                super.onRestart();
+
+                System.out.println( "Método onRestart() executado." );
+     }
+
+
+
+    @Override
+    protected void onResume() {
+                // TODO Auto-generated method stub
+                super.onResume();
+
+               System.out.println( "Método onResume() executado." );
+    }
+
+
+
+    @Override
+    protected void onStart() {
+                // TODO Auto-generated method stub
+                super.onStart();
+                        System.out.println( "Método onStart() executado." );
+    }
+
+             @Override
+     protected void onStop() {
+                // TODO Auto-generated method stub
+                super.onStop();
+
+                System.out.println( "Método onStop() executado." );
+          }
+
+            }
+
+
+
